a5c5b6ea72945b67ae89e3c83fdc6da41d9a793e
20TwentyVizion <pyravizion@gmail.com>
2024-12-28 15:52:04 -0500

    Fix Dialog component imports and structure

--- components/code-dialog.tsx
+++ components/code-dialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as Dialog from "@radix-ui/react-dialog";
+import { Dialog } from "@radix-ui/react-dialog";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { motion } from "framer-motion";
 import { useState } from "react";
@@ -47,7 +47,7 @@ export default function CodeDialog({
   };
 
   return (
-    <Dialog.Root open={isOpen} onOpenChange={() => onClose()}>
+    <Dialog.Root open={isOpen} onOpenChange={onClose}>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/50 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
         <Dialog.Content className="fixed left-[50%] top-[50%] flex h-[90vh] w-[90vw] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-xl bg-white p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]">
