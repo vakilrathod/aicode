beb3689baecd2f00983de4feeedf92c3cb677baf
20TwentyVizion <pyravizion@gmail.com>
2024-12-30 18:42:45 -0500

    Remove halo background image from main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import bgImg from "@/public/halo.png";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
@@ -10,15 +8,6 @@ export default function Layout({
 }>) {
   return (
     <body className="bg-brand antialiased">
-      <div className="absolute inset-x-0 flex justify-center">
-        <Image
-          src={bgImg}
-          alt=""
-          className="w-full max-w-[1200px] mix-blend-screen"
-          priority
-        />
-      </div>
-
       <div className="isolate">
         <div className="mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-center py-2">
           <Header />
